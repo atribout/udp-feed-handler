0703d29ece568aafbbc6fec74da00694d38f9814
atribout <75943757+atribout@users.noreply.github.com>
2025-12-07 19:47:52 +0100

    Percentiles, gap, depth metrics added (#5)

--- main.cpp
+++ main.cpp
@@ -5,12 +5,14 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <immintrin.h>
 
 #include "OrderBook.h"
 #include "Order.h"
 #include "Listeners.h"
 #include "include/Messages.h"
 #include "include/RingBuffer.h"
+#include "include/TSCClock.h"
 
 constexpr size_t BUFFER_SIZE = 4096;
 RingBuffer<QueueItem, BUFFER_SIZE> ringBuffer;
@@ -18,16 +20,42 @@ std::atomic<bool> running{true};
 
 void consumer_thread()
 {
+    TSCClock::get().printCalibration();
     std::cout << "Engine started (waiting for data)...\n";
 
-    ConsoleListener listener;
-    OrderBook<ConsoleListener> lob(listener);
+    EmptyListener listener;
+    OrderBook<EmptyListener> lob(listener);
 
-    while(running)
+    // We store the latencies to print the percentiles later
+    std::vector<uint64_t> samples;
+    samples.reserve(100000);
+
+    uint64_t maxQueueDepth = 0;
+
+    // For packet loss
+    uint64_t lastSeqNum = 0;
+    uint64_t gapCount = 0;
+
+    while (running) 
     {
+        size_t currentDepth = ringBuffer.getSize();
+        if (currentDepth > maxQueueDepth) maxQueueDepth = currentDepth;
+
         QueueItem* item = ringBuffer.peek();
-        if (item)
+        if (item) 
         {
+            if(item->seqNum <= lastSeqNum)
+            {
+                lastSeqNum = item->seqNum;
+            }
+            else if(lastSeqNum != 0 && item->seqNum != lastSeqNum + 1)
+            {
+                gapCount += (item->seqNum -lastSeqNum -1);
+            }
+            lastSeqNum = item->seqNum;
+
+            uint64_t start_cycles = rdtsc();
+            // --- CRITICAL ZONE ---
             if(item->type == MsgType::AddOrder)
             {
                 Side side = (item->side == 'B') ? Side::Buy : Side::Sell;
@@ -38,11 +66,37 @@ void consumer_thread()
             {
                 lob.cancelOrder(item->id);
             }
+            // -----------------------------------------
+
+            uint64_t end_cycles = rdtsc();
+
             ringBuffer.advance();
-        }
-        else
+
+            uint64_t cycles = end_cycles - start_cycles;
+            samples.push_back(cycles);
+
+            if (samples.size() > 100000) {
+
+                std::sort(samples.begin(), samples.end());
+
+                double p50 = TSCClock::get().toNanos(samples[50000]);
+                double p99 = TSCClock::get().toNanos(samples[99000]);
+                double maxLat = TSCClock::get().toNanos(samples.back());
+
+                std::cout << "--- STATS REPORT ---\n"
+                          << "Lat p50   : " << p50 << " ns\n"
+                          << "Lat p99   : " << p99 << " ns\n"
+                          << "Lat Max   : " << maxLat << " ns\n"
+                          << "Queue Max : " << maxQueueDepth << " / " << BUFFER_SIZE << "\n"
+                          << "Packet Loss : " << gapCount << "\n";
+
+                samples.clear();
+                maxQueueDepth = 0;
+            }
+        } 
+        else 
         {
-            std::this_thread::yield();
+            _mm_pause();
         }
     }
 }
@@ -74,7 +128,7 @@ int main()
         return -1;
     }
 
-    std::cout << "Newtork thread listening on port 1234..." << std::endl;
+    std::cout << "Network thread listening on port 1234..." << std::endl;
 
     char buffer[1024];
     socklen_t len = sizeof(cliaddr);
@@ -85,18 +139,17 @@ int main()
 
         if (n > 0)
         {
-            MsgType type = static_cast<MsgType>(buffer[0]);
+            if (n < sizeof(PacketHeader)) continue;
+
+            PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
 
             QueueItem* slot = ringBuffer.claim();
-            if(!slot)
-            {
-                std::cerr << "Buffer FULL\n";
-                continue;
-            }            
+            if(!slot) continue;
 
-            slot->type = type;
+            slot->type = header->type;
+            slot->seqNum = header->seqNum;
 
-            if(type == MsgType::AddOrder)
+            if(header->type == MsgType::AddOrder)
             {
                 if (n < sizeof(AddOrderMsg)) continue;
 
@@ -108,7 +161,7 @@ int main()
 
                 ringBuffer.publish();
             }
-            else if(type == MsgType::CancelOrder)
+            else if(header->type == MsgType::CancelOrder)
             {
                 if (n < sizeof(CancelOrderMsg)) continue;
 
--- market_sim.py
+++ market_sim.py
@@ -12,58 +12,50 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 live_orders = [] 
 current_price = 10000 # Price in cents (100.00)
 order_id_counter = 1
+seq_num = 1
 
 print(f"Market simulator started on {MCAST_PORT}...")
-print("Generating volatility and micro-bursts...")
 
 try:
     while True:
-        # --- 1. MICRO-BURST SIMULATION ---
-        # Send between 10 and 100 orders in a rapid burst
         burst_size = random.randint(10, 100)
         
         for _ in range(burst_size):
-            
-            # --- 2. PRICE RANDOM WALK ---
-            # Price moves by -5, 0, or +5 cents
             move = random.choices([-5, 0, 5], weights=[0.3, 0.4, 0.3])[0]
             current_price += move
             if current_price < 100: current_price = 100 # Price floor
 
-            # --- 3. DECISION: ADD OR CANCEL? ---
-            # 80% chance to Add, 20% to Cancel (if we have live orders)
             msg_type = b'A'
             if len(live_orders) > 0 and random.random() < 0.2:
-                msg_type = b'C' # Cancel
+                msg_type = b'C'
             
             packet = None
             
             if msg_type == b'A':
-                # --- BUILD ADD ORDER ---
                 qty = random.randint(1, 100)
                 side = b'B' if random.random() > 0.5 else b'S'
                 
-                packet = struct.pack('=cQiIc', b'A', order_id_counter, current_price, qty, side)
+                packet = struct.pack('=QcQiIc', seq_num, b'A', order_id_counter, current_price, qty, side)
                 
                 live_orders.append(order_id_counter)
                 order_id_counter += 1
                 
             else:
-                # --- BUILD CANCEL ORDER ---
                 target_id = random.choice(live_orders)
                 live_orders.remove(target_id)
                 
-                packet = struct.pack('=cQ', b'C', target_id)
+                packet = struct.pack('=QcQ', seq_num, b'C', target_id)
 
             sock.sendto(packet, (MCAST_GRP, MCAST_PORT))
+            seq_num += 1
 
         # --- 4. PAUSE BETWEEN BURSTS ---
         # Sleep briefly (1ms to 10ms) to let the C++ consumer catch up
         # This creates the "sawtooth" pattern
         time.sleep(random.uniform(0.001, 0.010)) 
 
-        if order_id_counter % 1000 == 0:
-            print(f"Stats: {order_id_counter} msgs sent. Current Price: {current_price}")
+        if seq_num % 1000 == 0:
+            print(f"Stats: {seq_num} msgs sent. Current Price: {current_price}")
 
 except KeyboardInterrupt:
     print("\n Simulation stopped.")
